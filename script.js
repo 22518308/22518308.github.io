99249dfa292d657943ba1d0e3f1c36c5a19caa41
22518308 <22518308@stu.mmu.ac.uk>
2025-09-26 04:46:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+﻿function copyText(value) {
+    var copyText = document.getElementById(value).innerText;
+    navigator.clipboard.writeText(copyText);
+    document.getElementById("copy" + value).src = "images/tick_icon.png";
+    setTimeout(function () { document.getElementById("copy" + value).src = "images/clipboard_icon.png"; }, 300)
+}
