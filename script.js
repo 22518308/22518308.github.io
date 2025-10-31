36d53b8e8592a9aacc3037c6e72811f7f6a555e5
22518308 <22518308@stu.mmu.ac.uk>
2025-10-31 02:24:16 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,28 @@
     document.getElementById("copy" + value).src = "images/tick_icon.png";
     setTimeout(function () { document.getElementById("copy" + value).src = "images/clipboard_icon.png"; }, 300)
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const projectItems = document.querySelectorAll(".project-item");
+
+    if (projectItems[0]) {
+        projectItems[0].classList.add("visible");
+    }
+
+    const observer = new IntersectionObserver((entries, obs) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+                obs.unobserve(entry.target); 
+            }
+        });
+    }, {
+        threshold: 0.6 //% of project visible to trigger
+    });
+
+    projectItems.forEach((item, index) => {
+        if (index > 0) {
+            observer.observe(item);
+        }
+    });
+});
